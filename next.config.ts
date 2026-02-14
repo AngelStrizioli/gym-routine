77032bdfddc00feb74b9638b51cfe91d5714beac
Angel Strizioli <astrizioli@gmail.com>
2026-02-14 15:00:08 -0300

    fix: next vulnerability

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-// next.config.ts
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "src"), // "@/..." -> "<root>/src/..."
-    };
-    return config;
+  turbopack: {
+    root: __dirname,
   },
 };
 
